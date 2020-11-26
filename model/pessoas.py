447c2c4641b3093e85ba5633d3513d6895244c24
leandro almeida <leandroalmeida@pop-os.localdomain>
2020-11-25 21:38:49 -0300

    algumas alteraçoes iniciais

--- model/pessoas.py
+++ model/pessoas.py
@@ -1,14 +1,18 @@
+import calculos
+
 class Pessoa(object):
     """
     Esse é o modelo de dados das pessoas que se cadastrarão no sistema
     """
 
-    def __init__(self, nome, cpf, endereco, nE = 0):
+    def __init__(self, nome, cpf, endereco, nasc, numE=0):
         self.nome = nome
         self.cpf = cpf
         self.endereco = endereco 
-        self.numeroDeEstudante = nE
+        self.numeroDeEstudante = numE
+        self.dataDeNascimento = nasc
         self.seEAluno = False if self.numeroDeEstudante == 0 else True
+        self.valor = 15
 
 
     def salvar(self):
@@ -21,7 +25,19 @@ class Pessoa(object):
     def alterar(self):
         pass
 
+#Retorna o valor a ser pago
+    def valorAPagar(self):
+        if self.seEAluno:
+            valoAPagar = calculos.calculaDesconto(self.valor)
+        else:
+            valoAPagar = self.valor
+
+        return valoAPagar
+
+a = Pessoa("Juvelino", 16498765467, "hguhu 354687", "15/12/2000", 123456879)
+print(a.valorAPagar())
 
 
-a = Pessoa("Gustavo", 135468798431, 32654898774654, "Tairu")
-a.salvar()
+
+
+
